9bb6eef7484acfc44b69d333b9d2707bffa7386a
alstondsouza1 <alstondsouza35@gmail.com>
2025-12-06 01:03:34 -0800

    testing the end to end testing on the CI

--- sal-tracker/frontend/cypress/e2e/videoList.cy.js
+++ sal-tracker/frontend/cypress/e2e/videoList.cy.js
@@ -5,10 +5,12 @@ describe("Home Page", () => {
 
   it("should go to video list, then EnsantinaClippedPreview", () => {
     const fileName = "ensantinaClipped.mp4";
+
     cy.contains("Start Processing").click();
     cy.url().should("include", "/videos");
 
-    cy.contains(fileName).click();
+    // Ensure videos have loaded
+    cy.contains(fileName, { timeout: 8000 }).click();
 
     cy.url().should("include", `preview/${fileName}`);
   });
