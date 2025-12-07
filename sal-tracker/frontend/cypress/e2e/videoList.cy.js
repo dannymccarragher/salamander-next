7a0d65bb56f89cfc93aa828a9681d6a49112ed7f
alstondsouza1 <alstondsouza35@gmail.com>
2025-12-06 21:46:45 -0800

    added mock /api/videos in videoList Cypress test

--- sal-tracker/frontend/cypress/e2e/videoList.cy.js
+++ sal-tracker/frontend/cypress/e2e/videoList.cy.js
@@ -1,17 +1,31 @@
+// cypress/e2e/videoList.cy.js
+
 describe("Home Page", () => {
   beforeEach(() => {
+    // 🔹 Mock the backend /api/videos response
+    cy.intercept("GET", "**/api/videos", {
+      statusCode: 200,
+      body: ["ensantinaClipped.mp4"],
+    }).as("mockVideos");
+
+    // Visit home page
     cy.visit("/");
   });
 
   it("should go to video list, then EnsantinaClippedPreview", () => {
     const fileName = "ensantinaClipped.mp4";
 
+    // Click Start Processing to go to /videos
     cy.contains("Start Processing").click();
     cy.url().should("include", "/videos");
 
-    // Ensure videos have loaded
+    // Wait until the mocked videos list is loaded
+    cy.wait("@mockVideos");
+
+    // Now the page should show ensantinaClipped.mp4
     cy.contains(fileName, { timeout: 8000 }).click();
 
+    // Check that we navigated to the correct preview page
     cy.url().should("include", `preview/${fileName}`);
   });
 });
