81424f37bf66aea9caa333d740472769f80f351d
Danny McCarragher <dannymccarragher@gmail.com>
2025-12-06 20:57:34 -0800

    mock api call instead of trying to use actual mp4 file

--- sal-tracker/frontend/cypress/e2e/videoList.cy.js
+++ sal-tracker/frontend/cypress/e2e/videoList.cy.js
@@ -1,12 +1,22 @@
 describe("Home Page", () => {
+  const fileName = "ensantinaClipped.mp4";
+
   beforeEach(() => {
+    // Mock the /api/videos endpoint to return our test video
+    cy.intercept("GET", "**/api/videos", {
+      statusCode: 200,
+      body: [fileName],
+    }).as("getVideos");
+
     cy.visit("/");
   });
 
   it("should go to video list, then EnsantinaClippedPreview", () => {
-    const fileName = "ensantinaClipped.mp4";
     cy.contains("Start Processing").click();
     cy.url().should("include", "/videos");
+
+    // Wait for the mocked API call to complete
+    cy.wait("@getVideos");
 
     cy.contains(fileName).click();
 
